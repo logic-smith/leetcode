743e6244a5e58e2b3d11e877e700eaa1c1d8499d
Ashish Ranjan <findme.ashishranjan@gmail.com>
2026-01-02 10:22:38 +0530

    Time: 21 ms (11.26%) | Memory: 47.3 MB (75.13%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,15 +1,10 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
         //storing frequency in map
+        int n = nums.length/2;
         Map<Integer, Integer> frequencyMap = new HashMap<>();
         for(int num: nums){
             frequencyMap.put(num, frequencyMap.getOrDefault(num,0)+1);
-        }
-
-        int n = nums.length/2;
-        
-        //final traversal
-        for(int num: nums){
             if(frequencyMap.get(num) == n){
                 return num;
             }
