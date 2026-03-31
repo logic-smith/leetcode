4ad8fc847a5b53da6d3fa9edf19ec3b10f20560b
Ashish Ranjan <findme.ashishranjan@gmail.com>
2026-03-31 10:06:19 +0530

    Time: 6 ms (26.01%) | Memory: 43.3 MB (78.74%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h=0;
+        int n=citations.length;
+        for(int i=n-1; i>=0; i--){
+            if(citations[i] >= n-i){
+                h = n-i;
+            }else{
+                break;
+            }
+        }
+        return h;
+    }
+}
